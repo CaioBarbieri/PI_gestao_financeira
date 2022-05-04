e18aa3671a3e5143dca751c2f98e4d4e7ac93c93
Caio Barbieri <caiozitos2014@gmail.com>
2022-05-04 20:15:08 -0300

    teste de commit

--- app/src/main/java/com/example/pigestofinanceira/TesteActivity.kt
+++ app/src/main/java/com/example/pigestofinanceira/TesteActivity.kt
@@ -3,7 +3,7 @@ package com.example.pigestofinanceira
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class TesteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
